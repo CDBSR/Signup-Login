3f1525b5f806d225212167f20d7bb9588440e7e5
BrajsundariRathor <rathorbrajsundari@gmail.com>
2024-12-08 11:49:36 +0530

    add login function

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ function signup(userName) {
     }
 }
 
+// function for feature/login
+
+function login(userName, password) {
+    const correctPassword = "Emp@123";
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    } else if (password !== correctPassword) {
+        return "Wrong Password...";
+    } else {
+        return "Login Successful...";
+    }
+}
+
 console.log(signup("john_doe")); 
 console.log(signup("user")); 
 console.log(signup("user"));
+console.log(login("john_doe", "Emp@123"));
+console.log(login("user2","Emp@123"))
+console.log(login("user", "emp"))
